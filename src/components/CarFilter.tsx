032db78e1bf7411e480b461eb26901e7d373f9aa
Efim Konnov <youpifun@mail.ru>
2021-05-26 14:35:18 +0300

    Добавлены фильтры. Реализована функция rangeFilterer.

--- src/components/CarFilter.tsx
+++ src/components/CarFilter.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Table, TableColumn, TableRow, TableFilters } from '@consta/uikit/Table';
-import {TableTextFilter} from '@consta/uikit/__internal__/src/components/Table/TextFilter/TableTextFilter';
+import { TableTextFilter } from '@consta/uikit/__internal__/src/components/Table/TextFilter/TableTextFilter';
+import { TableNumberFilter } from '@consta/uikit/__internal__/src/components/Table/NumberFilter/TableNumberFilter';
 import data from '../assets/data.json'
 import columns_config from '../assets/columns_config.json'
 
@@ -19,26 +20,30 @@ type CustomTableRow = TableRow & {
 
 type CustomIndexableTableRow = IIndexable & CustomTableRow;
 
-function getUniqueByFieldFilterObjects(field: string, arr: CustomIndexableTableRow[]) {
-    return Array.from(new Set(arr.map(element => element[field]))).map(element => Object({name: element, value: element}));
-}
 
 class CarFilter extends React.Component {
-    render() {
+    getUniqueFieldsForTextFilter(field: string, arr: CustomIndexableTableRow[]) {
+        return Array.from(new Set(arr.map(element => element[field]))).map(element => Object({name: element, value: element}));
+    }
 
+    rangeFilterer(cellValue:number, filterValues: {min: number, max: number}) : boolean {
+        return cellValue>=filterValues.min&&cellValue<=filterValues.max;
+    }
+
+    render() {
         const filters: TableFilters<CustomTableRow> = [ 
             {
                 id: 'brand',
                 name: 'Brand: ',
                 field: 'brand',
                 filterer: (cellValue, filterValues: Array<{ value: string; name: string }>) => {
-                return filterValues.some((filterValue) => filterValue && filterValue.value === cellValue);
+                    return filterValues.some((filterValue) => filterValue && filterValue.value === cellValue);
                 },
                 component: {
                     name: TableTextFilter,
                     props: {
                         withSearch: true,
-                        items: getUniqueByFieldFilterObjects('brand', data)
+                        items: this.getUniqueFieldsForTextFilter('brand', data)
                     },
                 }, 
             },
@@ -47,14 +52,66 @@ class CarFilter extends React.Component {
                 name: 'Model: ',
                 field: 'model',
                 filterer: (cellValue, filterValues: Array<{ value: string; name: string }>) => {
-                return filterValues.some((filterValue) => filterValue && filterValue.value === cellValue);
+                    return filterValues.some((filterValue) => filterValue && filterValue.value === cellValue);
                 },
                 component: {
                     name: TableTextFilter,
                     props: {
                         withSearch: true,
-                        items: getUniqueByFieldFilterObjects('model', data)
+                        items: this.getUniqueFieldsForTextFilter('model', data)
                     },
+                },
+            },
+            {
+                id: 'year',
+                name: 'Year range: ',
+                field: 'year',
+                filterer: (cellValue, filterValues: {min: number, max: number}) => {
+                    return this.rangeFilterer(cellValue, filterValues);
+                },
+                component: {
+                    name: TableNumberFilter,
+                },
+            },
+            {
+                id: 'fuel',
+                name: 'Fuel: ',
+                field: 'fuel',
+                filterer: (cellValue, filterValues: Array<{ value: string; name: string }>) => {
+                    return filterValues.some((filterValue) => filterValue && filterValue.value === cellValue);
+                },
+                component: {
+                    name: TableTextFilter,
+                    props: {
+                        withSearch: false,
+                        items: this.getUniqueFieldsForTextFilter('fuel', data)
+                    },
+                },
+            },
+            {
+                id: 'type',
+                name: 'Body type: ',
+                field: 'bodyType',
+                filterer: (cellValue, filterValues: Array<{ value: string; name: string }>) => {
+                    return filterValues.some((filterValue) => filterValue && filterValue.value === cellValue);
+                },
+                component: {
+                    name: TableTextFilter,
+                    props: {
+                        withSearch: false,
+                        items: this.getUniqueFieldsForTextFilter('bodyType', data)
+                    },
+                },
+            },
+            {
+                id: 'price',
+                name: 'Price: ',
+                field: 'price',
+                filterer: (cellValue, filterValues: {min: number, max: number}) => {
+                    return this.rangeFilterer(cellValue, filterValues);
+                },
+                component: {
+                    name: TableNumberFilter,
                 },
             },
         ]
